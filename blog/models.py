614b92612bf208370967d0227741a7b98f3ec797
ajbsarmi <ardenjohn.sarmiento@gmail.com>
2021-05-22 08:41:00 +0800

    add jobs in homepage

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models.fields import TextField
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = TextField()
+    image = models.ImageField(upload_to ='images/')
+    
+
