03f86f0783ef70b853d13c59951a21699f9cc7d7
ajbsarmi <ardenjohn.sarmiento@gmail.com>
2021-05-24 15:31:13 +0800

    polished website

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 from django.db.models.fields import TextField
 
+
 class Blog(models.Model):
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField()
     body = TextField()
     image = models.ImageField(upload_to ='images/')
-    
 
+    #This returns the title of the blog page in admin/blog/blog
+    def __str__(self):
+        return self.title
+
+    #This limit the the number of the next in the body
+    def summary(self):
+        return self.body[:100]
+
+    #This will only return the date in the blog page
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
